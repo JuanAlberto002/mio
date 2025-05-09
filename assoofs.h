de012b2b9b650fdb626f15951ead26fa478a9b3b
JuanAlberto002 <160509651+JuanAlberto002@users.noreply.github.com>
2025-05-09 12:33:19 +0200

    Create assoofs.h

---
+++ assoofs.h
@@ -0,0 +1,51 @@
+#ifndef ASSOOFS_H
+#define ASSOOFS_H
+
+#include <stdint.h>
+#include <linux/stat.h>
+
+#define ASSOOFS_MAGIC 0x20200406
+#define ASSOOFS_DEFAULT_BLOCK_SIZE 4096
+#define ASSOOFS_FILENAME_MAXLEN 255
+
+#define ASSOOFS_SUPERBLOCK_BLOCK_NUMBER 0
+#define ASSOOFS_INODESTORE_BLOCK_NUMBER 1
+#define ASSOOFS_ROOTDIR_BLOCK_NUMBER 2
+#define ASSOOFS_ROOTDIR_INODE_NUMBER 0
+
+#define ASSOOFS_MAX_FILESYSTEM_OBJECTS_SUPPORTED 64
+
+#define ASSOOFS_LAST_RESERVED_BLOCK ASSOOFS_ROOTDIR_BLOCK_NUMBER
+#define ASSOOFS_LAST_RESERVED_INODE ASSOOFS_ROOTDIR_INODE_NUMBER
+
+const int ASSOOFS_TRUE = 1;
+const int ASSOOFS_FALSE = 0;
+
+struct assoofs_super_block_info {
+	uint64_t version;
+	uint64_t magic;
+	uint64_t block_size;
+	uint64_t inodes_count;
+	uint64_t free_blocks;
+	uint64_t free_inodes;
+	char padding[4048];
+};
+
+struct assoofs_dir_record_entry {
+	char filename[ASSOOFS_FILENAME_MAXLEN];
+	uint64_t inode_no;
+	uint64_t entry_removed;
+};
+
+struct assoofs_inode_info {
+	mode_t mode;
+	uint64_t inode_no;
+	uint64_t data_block_number;
+	union {
+    	uint64_t file_size;
+    	uint64_t dir_children_count;
+	};
+};
+
+#endif
+
