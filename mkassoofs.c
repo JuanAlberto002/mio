4f4e3c59f1b7af01799e51c5f9214a591ccdab82
JuanAlberto002 <160509651+JuanAlberto002@users.noreply.github.com>
2025-06-24 06:44:56 -0400

    Update mkassoofs.c

--- mkassoofs.c
+++ mkassoofs.c
@@ -3,7 +3,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <linux/types.h> 
+#include <linux/types.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include "assoofs.h"
